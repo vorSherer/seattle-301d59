53ef5f83400c0fb86189a2c16dce7fb272588a43
LENA EIVY <applena@gmail.com>
2020-03-25 17:10:43 -0700

    day 13 updated

---
+++ class-13/demo/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO tasks (title, contact, status, category, description) 
+VALUES('finish all the labz', 'yasir', 'not done', 'homework', 'do the work');
+
+INSERT INTO tasks (title, contact, status, category, description) 
+VALUES('turn in late assignments', 'alex', 'not done', 'homework', 'do the work');
+
+SELECT * FROM tasks;
